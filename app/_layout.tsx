806811d86fa8d48ca9c7cd23fb128083b420a54a
RenMan95 <manna.renn@gmail.com>
2026-03-10 14:05:23 +0100

    Create _layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Stack } from 'expo-router';
+import { AuthProvider } from '../context/AuthContext';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <StatusBar style="light" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </AuthProvider>
+  );
+}
