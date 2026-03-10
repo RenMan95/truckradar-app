f777039d51020a50877e73ea586b8413efc84cc9
RenMan95 <manna.renn@gmail.com>
2026-03-10 14:09:44 +0100

    Create _layout.tsx

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: '#1A1A2E' },
+      }}
+    >
+      <Stack.Screen name="welcome" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
+  );
+}
